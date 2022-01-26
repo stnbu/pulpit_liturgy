5395c357714ec73930dc15a6c76e758879265fe1
Mike Burr <mb@unintuitive.org>
2022-01-26 07:35:51 -0700

    black

--- 1_elliptic_curves.py
+++ 1_elliptic_curves.py
@@ -7,22 +7,22 @@ import astoroid
 
 extent = 12
 
-class FFEC:
 
+class FFEC:
     def __init__(self, A, B, P):
         self.A = A
         self.B = B
         self.P = P
-        self.lhs = lambda y: y**2
-        self.rhs = lambda x: x**3 + self.A*x + self.B
+        self.lhs = lambda y: y ** 2
+        self.rhs = lambda x: x ** 3 + self.A * x + self.B
         self.implicit = lambda x, y: self.rhs(x) - self.lhs(y)
         self.parametric = lambda x: math.sqrt((x) ** 3 + (self.A * (x)) + self.B)
 
     def get_tex(self):
         return r"y^2 = x^3 + %dx + %d over \mathbb{Z}_%d" % (self.A, self.B, self.P)
 
-class FFECPlotter(Scene):
 
+class FFECPlotter(Scene):
     def __init__(self, A, B, P, *args, **kwargs):
         self.P = Decimal(P)
         self.ec = FFEC(A, B, P)
@@ -40,14 +40,22 @@ class FFECPlotter(Scene):
         points = []
         for i, n in enumerate(astoroid.fdrange(-0.77, 50, 0.01)):
             points.append((n, self.ec.parametric(n)))
-        modular_points = [[astoroid.ModularNumber(Decimal(n), self.P) for n in point] for point in points]
+        modular_points = [
+            [astoroid.ModularNumber(Decimal(n), self.P) for n in point]
+            for point in points
+        ]
         lines = astoroid.get_lines(modular_points, self.P)
         colors = list(astoroid.gen_color_gradient((255, 0, 0), (0, 0, 255), len(lines)))
         for j, line in enumerate(lines):
-            color = list(astoroid.gen_color_gradient((23, 99, 142), (184, 108, 2), len(lines)))
+            color = list(
+                astoroid.gen_color_gradient((23, 99, 142), (184, 108, 2), len(lines))
+            )
             modular_porabola = VGroup(color=colors[j], stroke_width=3)
-            modular_porabola.set_points_as_corners([astoroid.to_manim_point(*l) for l in line])
+            modular_porabola.set_points_as_corners(
+                [astoroid.to_manim_point(*l) for l in line]
+            )
             self.add(modular_porabola)
+
 
 if __name__ == "__main__":
     scene = FFECPlotter(2, 2, 11)
